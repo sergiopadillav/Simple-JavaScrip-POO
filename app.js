6264922153228c7af6705662b7a144246edac3b2
Sergio Padilla <sergiolpv97@gmail.com>
2019-07-03 16:26:27 -0700

    App Finished!

--- app.js
+++ app.js
@@ -33,7 +33,25 @@ class UI{
 
   }
 
-  showMessage(){
+  showMessage(message,cssClass){
+    const div = document.createElement("div");
+
+    if(cssClass === "success"){
+      div.className = `alert alert-${cssClass} mt-2`;
+    } else {
+      div.className = `alert alert-${cssClass} mt-2`;
+    }
+
+    div.appendChild(document.createTextNode(message));
+
+    //DOM
+    const container = document.querySelector(".container");
+    const app = document.querySelector("#app");
+    container.insertBefore(div,app)
+    setTimeout(function(){
+      document.querySelector(".alert").remove()
+    },2500)
+
 
   }
 
@@ -43,6 +61,8 @@ class UI{
 }
 
 //DOM EVENTS
+
+//CAPTURANDO EL EVENTO DEL INPUT PARA GUARDAR PRODUCTO
 document.getElementById("product-form")
 .addEventListener("submit", function(e){
   const name = document.getElementById("name").value;
@@ -53,13 +73,20 @@ document.getElementById("product-form")
 
   const ui = new UI();
 
+        if (name === '' || price === '' || year === '') {
+            return ui.showMessage('Please Insert data in all fields!', 'danger');
+        }
+
   ui.addProduct(product);
   ui.resetForm();
+  ui.showMessage("Product Added Successfully!", "success")
 
   e.preventDefault();
 });
 
+// CAPTURANDO EL EVENTO DE ELIMINAR UN PRODUCTO Y MOSTRAR MENSAJE
 document.getElementById("product-list").addEventListener("click", function(e){
   const ui = new UI();
   ui.delateProduct(e.target)
+  ui.showMessage("Product Deleted Succsssfully", "info")
 })
